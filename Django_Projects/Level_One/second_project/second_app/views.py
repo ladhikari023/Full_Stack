38d7574aa4411f508a8d0513279dfe5cf3657b34
Laxman Adhikari <ladhikari023@unm.edu>
2021-11-24 15:28:35 -0700

    Level_One directory Committed

--- Django_Projects/Level_One/second_project/second_app/views.py
+++ Django_Projects/Level_One/second_project/second_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    my_dict2 = {'insert_second':"I am coming from views.py"}
+    my_dict2 = {'insert_second':"I am coming from views.py from changed Directory"}
     return render(request,'second_app/index.html',context=my_dict2)
 
 def home(request):
