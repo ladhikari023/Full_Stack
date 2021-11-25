8e09973cdd9aa8aef0fd05eaab7736c4a28000ee
Laxman Adhikari <ladhikari023@unm.edu>
2021-11-25 15:56:20 -0700

    Custom Template Tags Committed

--- Django_Projects/Level_Four/first_project/first_app/views.py
+++ Django_Projects/Level_Four/first_project/first_app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'first_app/index.html')
+    context_dict = {'text':"Some Text",'number':100}
+    return render(request,'first_app/index.html',context=context_dict)
 
 def other(request):
     return render(request,'first_app/other.html')
